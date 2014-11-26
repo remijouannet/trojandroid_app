9cd0585abf09daa4d92786ad5ce9f0c1133e2339
Jean-Laurent <toto@toto.fr>
2014-11-26 10:55:18 +0100

    commit1

---
+++ app/src/main/java/trojan/android/android_trojan/second_activity.java
@@ -0,0 +1,18 @@
+package trojan.android.android_trojan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jean-Laurent on 26/11/2014.
+ */
+public class second_activity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+    }
+}
