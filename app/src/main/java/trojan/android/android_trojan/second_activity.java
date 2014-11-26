e99da8479087e8e2ba19b26cb119faab24fd48a0
Remi <remijouannet@hotmail.fr>
2014-11-26 11:41:00 +0100

    dafZEG

--- app/src/main/java/trojan/android/android_trojan/second_activity.java
+++ app/src/main/java/trojan/android/android_trojan/second_activity.java
@@ -1,6 +1,11 @@
 package trojan.android.android_trojan;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 /**
@@ -8,11 +13,19 @@ import android.os.Bundle;
  */
 public class second_activity extends Activity {
 
-
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+        context = getApplicationContext();
 
     }
+
+    public void GetLocation(){
+        LocationManager locManager = (LocationManager)getSystemService(context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), false););
+    }
+
+
 }
